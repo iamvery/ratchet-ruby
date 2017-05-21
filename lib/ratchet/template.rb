985b68af83fd705b13f4ad914ed7c8c84d8ae43a
Jay Hayes <ur@iamvery.com>
2017-05-21 16:22:10 -0500

    Define a rails template that uses rails-specific generator

--- lib/ratchet/template.rb
+++ lib/ratchet/template.rb
@@ -3,4 +3,11 @@ require 'ratchet/engine'
 
 module Ratchet
   Template = Temple::Templates::Tilt(Engine)
+
+  if defined?(::ActionView)
+    RailsTemplate = Temple::Templates::Rails(
+      Engine,
+      generator: Temple::Generators::RailsOutputBuffer,
+    )
+  end
 end
