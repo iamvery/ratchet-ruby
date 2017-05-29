582b07b387ee2bda6d8355f514c68d937bd77b63
Jay Hayes <ur@iamvery.com>
2017-05-29 08:46:00 -0500

    Swap expectation order

--- test/lib/ratchet/data/attributes_test.rb
+++ test/lib/ratchet/data/attributes_test.rb
@@ -4,6 +4,6 @@ require 'ratchet/data/attributes'
 class AttributesDataTest < Minitest::Test
   def test_build
     attributes = Ratchet::Data::Attributes.new(class: 'active')
-    assert_equal attributes.build, ' class="active"'
+    assert_equal ' class="active"', attributes.build
   end
 end
